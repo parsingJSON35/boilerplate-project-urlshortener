8103ed5e488136d54c13d390b0427e844ef5f62c
Jason Campbell <jlcampbell86@gmail.com>
2019-10-30 09:44:45 -0400

    mongoose broken

--- server.js
+++ server.js
@@ -8,16 +8,20 @@ var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+/** this project needs a db !! **/
+
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,11 +29,18 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+
+// your first API endpoint...
+var Schema = mongoose.Schema
+
+var Url = new Schema({
+  original_url: String,
+  short_url: Number
+})
+
+app.post('/api/shorturl/new', (req, res) => {
+  res.json({original_url: req.body.url})
+})
 
 
 app.listen(port, function () {
